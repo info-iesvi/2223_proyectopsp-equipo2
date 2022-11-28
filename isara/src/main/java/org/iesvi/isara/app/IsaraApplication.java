bf9068c6f1970a8e555f0f0345cc507ee00dd72d
isagonrod <isabelmariagonzalezrodriguez@gmail.com>
2022-11-28 13:53:36 +0100

    PROJECT 'ISARA': project structure

---
+++ isara/src/main/java/org/iesvi/isara/app/IsaraApplication.java
@@ -0,0 +1,13 @@
+package org.iesvi.isara.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsaraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsaraApplication.class, args);
+    }
+
+}
