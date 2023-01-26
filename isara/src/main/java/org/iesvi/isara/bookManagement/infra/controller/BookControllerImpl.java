826853402d32d94108e7f2a8a8da3c82d05d7eb4
isagonrod <isabelmariagonzalezrodriguez@gmail.com>
2023-01-26 18:06:22 +0100

    ISSUE 6: Changes in the Book Controller layer

--- isara/src/main/java/org/iesvi/isara/bookManagement/infra/controller/BookControllerImpl.java
+++ isara/src/main/java/org/iesvi/isara/bookManagement/infra/controller/BookControllerImpl.java
@@ -1,20 +1,28 @@
 package org.iesvi.isara.bookManagement.infra.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.iesvi.isara.bookManagement.domain.Book;
 import org.iesvi.isara.bookManagement.infra.dto.BookDTO;
+import org.iesvi.isara.bookManagement.service.BookServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 /**
- * Controller layer of the Book Entity.
+ * Controller layer of the Book Entity that implements the BookController interface.
  * This serves as an intermediary between the BookServiceImpl class and the end user making the HTTP requests.
  *
  * @author Isa & Sara
  */
 @Controller
-public interface BookController {
+@RequiredArgsConstructor
+@RequestMapping("/isara")
+public class BookControllerImpl implements BookController {
+    @Autowired
+    private BookServiceImpl bookService;
 
     /**
      * GET method to obtain a list with all the books registered in the repository.
@@ -22,7 +30,9 @@ public interface BookController {
      * @return HTTP code 200 OK if successful.
      */
     @GetMapping("/books")
-    ResponseEntity<?> getAllBooks();
+    public ResponseEntity<?> getAllBooks() {
+        return ResponseEntity.ok(bookService.getAllBooks());
+    }
 
     /**
      * GET method to obtain the data of a book registered in the repository by id.
@@ -31,7 +41,9 @@ public interface BookController {
      * @return HTTP code 200 OK if successful.
      */
     @GetMapping("/book/{id}")
-    ResponseEntity<?> getBookById(@PathVariable Long id);
+    public ResponseEntity<?> getBookById(@PathVariable Long id) {
+        return ResponseEntity.ok(bookService.getBookById(id));
+    }
 
     /**
      * POST method to add a new book to the repository.
@@ -40,7 +52,9 @@ public interface BookController {
      * @return HTTP code 201 OK if created successfully.
      */
     @PostMapping("/book")
-    ResponseEntity<BookDTO> addNewBook(@Validated @RequestBody Book book);
+    public ResponseEntity<BookDTO> addNewBook(@Validated @RequestBody Book book) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(bookService.saveBook(book));
+    }
 
     /**
      * PUT method to modify the data of an existing book in the repository.
@@ -51,7 +65,10 @@ public interface BookController {
      * @return HTTP code 200 OK if the changes have been saved successfully.
      */
     @PutMapping("/book/{id}")
-    ResponseEntity<?> editBook(@RequestBody Book editBook, @PathVariable Long id);
+    public ResponseEntity<?> editBook(@RequestBody Book editBook, @PathVariable Long id) {
+        editBook.setIdBook(id);
+        return ResponseEntity.ok(bookService.saveBook(editBook));
+    }
 
     /**
      * DELETE method to remove a book from the repository by id.
@@ -60,5 +77,8 @@ public interface BookController {
      * @return HTTP code 202 No content if it was removed successfully.
      */
     @DeleteMapping("/book/{id}")
-    ResponseEntity<?> deleteBook(@PathVariable Long id);
+    public ResponseEntity<?> deleteBook(@PathVariable Long id) {
+        bookService.deleteBook(id);
+        return ResponseEntity.noContent().build();
+    }
 }
